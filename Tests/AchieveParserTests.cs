137f253a05e4f8ff38419d5ac824f35c8d014737
arkein <alex.zemn@gmail.com>
2016-05-16 12:22:39 +0300

    initial test project

---
+++ Tests/AchieveParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hearthstone_Treasury.Utils;
+
+namespace Tests
+{
+    [TestClass]
+    public class AchieveParserTests
+    {
+        [TestMethod]
+        public void ParseTest()
+        {
+            var result = AchieveParser.Parse(@"Resources\ACHIEVE.xml");
+        }
+    }
+}
---
+++ Tests/AchievementProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hearthstone_Treasury;
+using Hearthstone_Treasury.ViewModels;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class AchievementProviderTests
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var settings = new PluginSettingsViewModel { Locale = "ru-RU" };
+            var myFile = Path.GetFullPath(@"Resources\achievements_parsed.xml");
+            var blizzFile = Path.GetFullPath(@"Resources\achieve.xml");
+            var provider = AchievementProvider.Create(settings, myFile , blizzFile);
+        }
+    }
+}
